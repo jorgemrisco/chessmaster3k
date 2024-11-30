85a7e8c86977548cf6701831a9ba366912e49aad
Jorge M. Risco <jorgedelano7@gmail.com>
2024-11-30 16:09:35 -0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mainpage', component: MainpageComponent },
+  { path: 'iframepage', component: BoardComponent },
+  { path: '', redirectTo: '/mainpage', pathMatch: 'full' },
+  { path: '**', redirectTo: '/mainpage' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
